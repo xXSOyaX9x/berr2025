bd8e6ff152631cd81cc14fde21db2abf30b5c479
Brandon <brandonliew71@gmail.com>
2025-03-27 12:19:00 +0800

    created index.js with mongo

--- index.js
+++ index.js
@@ -1,32 +1,73 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require(`mongodb`);
+
+const drivers = [
+    {
+        name: "John Doey",
+        vehicleType: "Sedan",
+        isAvailable: true,  
+        rating: 4.8
+    },
+    {
+        name: "Alice Smith",
+        vehicleType: "SUV",
+        isAvailable: false,  
+        rating: 4.5
+    }
+];
+
+drivers.forEach(driver => console.log(driver.name));
+
+drivers.push({
+    name: "Brandon",
+    vehicleType: "Vellfire",
+    isAvailable: true,  
+    rating: 4.9
+});
+
+console.log(drivers);
 
 async function main() {
-    // Replace with your MongoDB connection string
-    const uri = "mongodb://localhost:27017";
+    const uri = "mongodb://localhost:27017"; 
     const client = new MongoClient(uri);
 
     try {
-
         await client.connect();
         console.log("Connected to MongoDB!");
 
         const db = client.db("testDB");
-        const collection = db.collection("users");
+        const collection = db.collection("drivers");
 
-        // Insert a document
-        await collection.insertOne({ name: "Brandon", birth: 2003 });
-        console.log("Document inserted!");
+        const driversCollection = db.collection("drivers");
 
-        // Query the document
-        const result = await collection.findOne({ name: "Brandon" });
-        console.log("Query result:", result);
+      //  for (const driver of drivers) {
+     //       const result = await driversCollection.insertOne(driver);
+      //      console.log(`New driver created: ${driver.name}, ID: ${result.insertedId}`);
+      //  }
 
-        console.time("Connection Time");
-        await client.connect();
-    console.timeEnd("Connection Time");
+      //  const updateResult = await db.collection('drivers').updateMany(
+      //     { name: "John Doey" },
+      //     [ { 
+      //          $set: { 
+      //                  rating: 
+      //                          { 
+      //                              $toDouble: { $round: [{ $add: ["$rating", 0.1] }, 1] } 
+       //                         }
+       //               } 
+        //      }
+       //    ]
+      //  );
+    
+     //  console.log(`Driver updated: Matched ${updateResult.matchedCount}, Modified ${updateResult.modifiedCount}`);
 
-    } catch (err) {
-        console.error("Error:", err);
+       const deleteResult = await db.collection(`drivers`).deleteMany({isAvailable: false});
+       console.log(`Deleted Count: ${deleteResult.deletedCount}`);
+
+     // const availableDrivers = await driversCollection.find({
+     //     isAvailable: true,
+     //     rating: {$gte: 4.5}
+     // }).toArray();
+     // console.log("Available drivers:", availableDrivers);
+
     } finally {
         await client.close();
     }
